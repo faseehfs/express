ef5290ed9bc9e7bc86a15f1fe2d5d65e88a3999b
FaseehFS <faseeh1080@gmail.com>
2026-02-23 08:50:26 +0530

    Create all users page

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,14 @@
 const router = require("express").Router();
+const connection = require("../db");
 
 router.get("/hello", (req, res) => {
   res.json({ message: "Hello from Express!" });
+});
+
+router.get("/all-users", (req, res) => {
+  connection.query("SELECT * FROM users", (err, rows, fields) => {
+    res.json({ rows: rows });
+  });
 });
 
 module.exports = router;
