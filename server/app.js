1b64d5bbc43b0069b38a017d327da9ab0675b2f9
FaseehFS <faseeh1080@gmail.com>
2026-03-02 08:41:00 +0530

    Use session

--- server/app.js
+++ server/app.js
@@ -31,7 +31,16 @@ app.use(express.static(path.join(__dirname, "../public")));
 // express.static(path) serves static files in the path.
 // If a matching file is found, it ends the request-response cycle.
 
-// The above middleware functions has no mount path, meaning they will execute
+app.use(express.urlencoded());
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: "shhh...",
+  }),
+);
+
+// The above middleware functions have no mount path, meaning they will execute
 // every time the app recieves a request.
 // However, the below middleware has a mount path.
 // Middleware functions are executed in the order in which they are defined.
