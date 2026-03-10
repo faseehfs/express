639d9f53caf050c7019fd1e9eadb370132dc2ebf
FaseehFS <faseeh1080@gmail.com>
2026-03-10 15:21:34 +0530

    Refactor server setup: move server logic to src/server.js and update package.json scripts

--- src/app.js
+++ src/app.js
@@ -1,18 +1,14 @@
-require("dotenv").config();
-
 const express = require("express");
-const path = require("path");
-const errorHandler = require("./middleware/errorHandler");
 const session = require("express-session");
+const path = require("path");
+
+const errorHandler = require("./middleware/errorHandler");
 const ensureAdminExists = require("./utils/ensureAdminExists");
 
 const app = express();
-const PORT = 3000;
 
-app.use(express.json()); // Only applies to requests whose Content-Type is application/json.
-
+app.use(express.json());
 app.use(express.urlencoded());
-
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
@@ -26,9 +22,6 @@ app.use(
 ensureAdminExists();
 
 app.use("/api", require("./routes"));
-
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-  console.log(`Running on http://localhost:${PORT}`);
-});
+module.exports = app;
